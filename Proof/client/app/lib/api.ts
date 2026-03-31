f915e6cee6742bf8a0025f0a252eb3af32600f6a
Rajat Maurya <rajatmaurya176@gmail.com>
2026-04-01 00:07:06 +0530

    fix:url Proff

--- Proof/client/app/lib/api.ts
+++ Proof/client/app/lib/api.ts
@@ -1,6 +1,6 @@
 // lib/api.ts
 export const getBlogs = async () => {
-  const res = await fetch("http://localhost:2000/api/images", {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/images`, {
     next: { revalidate: 60 },
   });
 
